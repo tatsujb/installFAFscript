9614cb2e3f62c7c3e8a55799a6815a07b7aa1882
tatsujb <tatsu@hotmail.fr>
2019-03-03 03:23:45 +0100

    Update installfaf.sh

--- installfaf.sh
+++ installfaf.sh
@@ -4,6 +4,7 @@ echo "steam user name :"
 read STEAMUSERNAME
 echo "steam password :"
 read -s STEAMPASSWORD
+sudo echo "now sudo"
 if [[ $(command -v java) ]] || [[ $(type -p java) ]] || [[ -n "$JAVA_HOME" ]] || [[ -x "$JAVA_HOME/bin/java" ]]
 then
 	echo "UH-OH, Java is already installed!"
@@ -100,13 +101,17 @@ fi
 cd
 mkdir faf
 cd faf
-wget https://github.com/FAForever/downlords-faf-client/releases/download/v0.9.3-beta/_dfc_unix_0_9_3-beta.tar.gz
+FAFVERSIONNUMBER=$(curl -v --silent https://api.github.com/repos/FAForever/downlords-faf-client/releases 2>&1 | grep '"tag_name": ' | head -n 1 | cut -f4,4 -d'"')
+TEMP=${VERSIONNUMBER:1}
+FAFVERSIONNUMBERNAME=${TEMP//./_}
+wget https://github.com/FAForever/downlords-faf-client/releases/download/$FAFVERSIONNUMBER/_dfc_unix_$FAFVERSIONNUMBERNAME.tar.gz
 tar -xpvzf _dfc_unix_0_9_3-beta.tar.gz
 mv downlords-faf-client-0.9.3-beta/{.,}* .
 rmdir downlords-faf-client-0.9.3-beta
 rm _dfc_unix_0_9_3-beta.tar.gz
 chmod +x downlords-faf-client && chmod +x lib/faf-uid
 cd
+echo 'PROTON_NO_ESYNC=1, PROTON_DUMP_DEBUG_COMMANDS=1 PROTON_USE_GALLIUM_NINE=1 PROTON_GALLIUM_NINE_MODULEPATH="/usr/lib/i386-linux-gnu/d3d/d3dadapter9.so.1:/usr/lib/x86_64-linux-gnu/d3d/d3dadapter9.so.1"%command%' >> ~/"the contents of this file are to be pasted in the forged alliance properties launch options"
 echo 'export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libnss3.so' >> ~/.bashrc
 echo 'export DEF_CMD=("/home/'$USER'/.steam/steam/steamapps/common/Supreme Commander Forged Alliance/bin/SupremeCommander.exe")' >> ~/.bashrc
 echo 'export TERM=xterm' >> ~/.bashrc
@@ -122,21 +127,33 @@ if [ $(command -v steam) ]
 then
     echo "steam is already installed, proceeding..."
 else
-    echo 'steam steam/question select "I AGREE"' | sudo debconf-set-selections
+    echo steam steam/question select "I AGREE" | sudo debconf-set-selections
     echo steam steam/license note '' | sudo debconf-set-selections
     sudo apt install -y steam
 fi
-steam -login ${STEAMUSERNAME} ${STEAMPASSWORD} -nofriendsui &
-sleep 3m
+mkdir -p ~/.steam/compatibilitytools.d
+https://api.github.com/repos/popsUlfr/Proton/releases
+PROTONVERSIONNUMBER=$(curl -v --silent https://api.github.com/repos/popsUlfr/Proton/releases 2>&1 | grep '"tag_name": ' | head -n 1 | cut -f4,4 -d'"')
+REPLACING=$(curl -v --silent https://api.github.com/repos/popsUlfr/Proton/releases 2>&1 | grep '"target_commitish": ' | head -n 1 | cut -f4,4 -d'"' | sed 's/[^_]\+/\L\u&/g')
+PROTONVERSION=${REPLACING/_G/-6_G}
+PROTONNAME=$PROTONVERSION"_"${PROTONVERSIONNUMBER##*-}
+wget https://github.com/popsUlfr/Proton/releases/download/$PROTONVERSIONNUMBER/$PROTONNAME.tar.xz
+
+tar xfv Proton_3.16-6_Gallium_Nine_Extras_0.3.0.tar.xz -C ~/.steam/compatibilitytools.d
+rm Proton_3.16-6_Gallium_Nine_Extras_0.3.0.tar.xz
+sudo apt-get install libd3dadapter9-mesa:i386 libd3dadapter9-mesa
+
+steam -nofriendsui -login ${STEAMUSERNAME} ${STEAMPASSWORD} -remember_password 
 if [ $(command -v steamcmd) ]
 then
     echo "steam CMD is already installed, proceeding..."
 else
-    echo 'steamcmd steam/question select "I AGREE"' | sudo debconf-set-selections
+    echo steamcmd steam/question select "I AGREE" | sudo debconf-set-selections
     echo steamcmd steam/license note '' | sudo debconf-set-selections
     sudo useradd -m steam
     cd /home/steam
     sudo apt install -y steamcmd
+	cd
 fi
 if [ $(command -v lib32gcc1) ]
 then
@@ -146,10 +163,7 @@ else
     sudo apt install -y lib32gcc1
 fi
 cd
-STEAMCMDLOCATION=$(find ~ -name 'steamcmd.sh' 2>/dev/null)
-STEAMCMDLOCATIONWHEREIS=$(whereis steamcmd | awk '{print $2}')
-echo "steam cmd sh  : ${STEAMCMDLOCATION}"
-echo "steam cmd whereis : ${STEAMCMDLOCATIONWHEREIS}"
+
 
 ####################################################################################################################
 #                                                                                                                  #
@@ -157,17 +171,5 @@ echo "steam cmd whereis : ${STEAMCMDLOCATIONWHEREIS}"
 #                                                                                                                  #
 ####################################################################################################################
 
-echo "........................................................................................................................"
-if [ ${STEAMCMDLOCATION} ]
-then
-    echo "finished steam CMD installation, steam CMD located at : ${STEAMCMDLOCATION}, logging in... "
-    echo "........................................................................................................................"
-    echo "installing Forged Alliance..."
-    echo "........................................................................................................................"
-    eval "${STEAMCMDLOCATION} +login ${STEAMUSERNAME} ${STEAMPASSWORD} +app_update 9420 validate +quit"
-    steam -applaunch 9420
-else
-    echo "finished steam CMD installation into weird path, steam CMD located at : ${STEAMCMDLOCATIONWHEREIS}, logging in... "
-    echo "........................................................................................................................"
-    echo "actually this part of the script is WIP, you'll have to launch FA download from steam yourself..."
-fi
+steamcmd +login ${STEAMUSERNAME} ${STEAMPASSWORD} +app_update 9420 validate +quit
+steam -applaunch 9420
