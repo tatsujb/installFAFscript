4007164baa5cebedd8f7f5a4f1379da90824eb12
tatsujb <tatsu@hotmail.fr>
2019-03-03 16:25:08 +0100

    version 1.0

--- installfaf.sh
+++ installfaf.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 # version 1.0
+STACKVERSION=1.0
+echo "new log file, fafSTACK version 1.0" >> ~/'fafstack-'$STACKVERSION'.log'
 echo "steam user name :"
 read STEAMUSERNAME
 echo "steam password :"
 read -s STEAMPASSWORD
-sudo echo "now sudo"
+
+sudo apt update &&
+echo "[$(date --rfc-3339=seconds)] now sudo" >> ~/'fafstack-'$STACKVERSION'.log'
+sudo apt -y dist-upgrade &&
+echo "[$(date --rfc-3339=seconds)] installing dependencies" >> ~/'fafstack-'$STACKVERSION'.log'
+
+sudo apt install libd3dadapter9-mesa:i386 libd3dadapter9-mesa
 echo 'PROTON_NO_ESYNC=1, PROTON_DUMP_DEBUG_COMMANDS=1, PROTON_USE_GALLIUM_NINE=1, PROTON_GALLIUM_NINE_MODULEPATH="/usr/lib/i386-linux-gnu/d3d/d3dadapter9.so.1:/usr/lib/x86_64-linux-gnu/d3d/d3dadapter9.so.1" %command%' >> ~/"the contents of this file are to be pasted in the forged alliance properties launch options"
 
 echo "installing steam and steam CMD..."
+echo "[$(date --rfc-3339=seconds)] installing steam and steam CMD..." >> ~/'fafstack-'$STACKVERSION'.log'
 if [ $(command -v steam) ]
 then
     echo "steam is already installed, proceeding..."
+    echo "[$(date --rfc-3339=seconds)] steam is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
 else
+    echo "[$(date --rfc-3339=seconds)] steam was not yet installed, installing..." >> ~/'fafstack-'$STACKVERSION'.log'
     echo steam steam/question select "I AGREE" | sudo debconf-set-selections
     echo steam steam/license note '' | sudo debconf-set-selections
     sudo apt install -y steam
@@ -26,7 +37,7 @@ tar xfv $PROTONNAME.tar.xz -C ~/.steam/compatibilitytools.d
 rm $PROTONNAME.tar.xz
 mv $PROTONNAME Proton
 cd
-sudo apt install libd3dadapter9-mesa:i386 libd3dadapter9-mesa
+
 #########################################################################################################################
 #                                                                                                                       #
 # WIP! have not figured out a way to enable proton for all games via command line, right now you have to do it manually #
@@ -34,11 +45,14 @@ sudo apt install libd3dadapter9-mesa:i386 libd3dadapter9-mesa
 #########################################################################################################################
 
 #BE SURE TO EDIT THIS VALUE ("210") ACCORDING TO YOUR INTERNET SPEED
-gnome-terminal -e "timeout -k 5 210 steam -nofriendsui -login ${STEAMUSERNAME} ${STEAMPASSWORD} -remember_password"
+gnome-terminal --tab --active -- bash -c 'timeout -k 5 210 steam -nofriendsui -login '$STEAMUSERNAME' '$STEAMPASSWORD' -remember_password'
+
 if [ $(command -v steamcmd) ]
 then
+    echo "[$(date --rfc-3339=seconds)] steam CMD is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
     echo "steam CMD is already installed, proceeding..."
 else
+    echo "[$(date --rfc-3339=seconds)] steam CMD was not yet installed, installing..." >> ~/'fafstack-'$STACKVERSION'.log'
     echo steamcmd steam/question select "I AGREE" | sudo debconf-set-selections
     echo steamcmd steam/license note '' | sudo debconf-set-selections
     sudo useradd -m steam
@@ -48,9 +62,11 @@ else
 fi
 if [ $(command -v lib32gcc1) ]
 then
+    echo "[$(date --rfc-3339=seconds)] lib32gcc1 is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
     echo "lib32gcc1 is already installed, proceeding..."
 else
     cd
+    echo "[$(date --rfc-3339=seconds)] lib32gcc1 was not yet installed, installing..." >> ~/'fafstack-'$STACKVERSION'.log'
     sudo apt install -y lib32gcc1
 fi
 cd
@@ -58,25 +74,31 @@ if [[ $(command -v java) ]] || [[ $(type -p java) ]] || [[ -n "$JAVA_HOME" ]] ||
 then
 	echo "UH-OH, Java is already installed!"
 	echo "This is suboptimal, crossing fingers for correct version Java version (E.G. version number 10)..."
+    	echo "[$(date --rfc-3339=seconds)] JAVA already installed!" >> ~/'fafstack-'$STACKVERSION'.log'
 	if [ $(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -f1 -d'.') -eq "10" ]
 	then
 		echo "Huzzah! you have the correct version of Java, here's hoping for the best!"
 		echo "Checking, if .bashrc config is also correct..."
+		echo "[$(date --rfc-3339=seconds)] specifically JAVA 10." >> ~/'fafstack-'$STACKVERSION'.log'
 		if grep -q "export INSTALL4J_JAVA_HOME=/usr/lib/jvm/jdk-10.0.2" ~/.bashrc
 		then
 			echo "Huzzah! Java looks all set, let's move on..."
+			echo "[$(date --rfc-3339=seconds)] satisfied with JAVA config." >> ~/'fafstack-'$STACKVERSION'.log'
         else
             echo "Hmmm... we're not looking to hot on .bashrc, let's see if we can fix that..."
+	echo "[$(date --rfc-3339=seconds)] but not correct config." >> ~/'fafstack-'$STACKVERSION'.log'
             if (whiptail --title "Entered : \"Java 10 present but .bashrc not correctly configured use-case\"" --yesno "Attempt .bashrc automated correct? \n\"No\" will close this script \n\"Yes\" will automatically edit bashrc \n(keep in mind this script was written by a donkey...)" 10 100)
 			then
 				echo export INSTALL4J_JAVA_HOME=/usr/lib/jvm/jdk-10.0.2 >> ~/.bashrc
 				source ~/.bashrc
 				echo "OK! \".bashrc\" edited!"
 				echo "assuming Java is all set, let's move on..."
+				echo "[$(date --rfc-3339=seconds)] corrected JAVA config." >> ~/'fafstack-'$STACKVERSION'.log'
 			else
 				echo "You're probably right to choose this..."
 				echo "please edit .bashrc yourself or remove java 10 and start over."
 				echo "exiting upon demand..."
+				echo "[$(date --rfc-3339=seconds)] Abandoned on user demand, JAVA was ill-configured" >> ~/'fafstack-'$STACKVERSION'.log'
 				exit 1
 			fi
             
@@ -88,10 +110,12 @@ then
         echo "The easliest sollution for you to use this script as-is, is for you to purge all java versions from your system (be sure to remove references in /etc/environment),"
         echo "then, re-run this script"
         echo "FAF-stack not installed, exiting..."
+	echo "[$(date --rfc-3339=seconds)] Incorrect JAVA version, exiting!" >> ~/'fafstack-'$STACKVERSION'.log'
         exit 1
 	fi
 else
 	echo "installing java 10..."
+	echo "[$(date --rfc-3339=seconds)] JAVA 10 installing..." >> ~/'fafstack-'$STACKVERSION'.log'
     
     wget https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz
     sudo mkdir /usr/lib/jvm
@@ -107,12 +131,13 @@ else
     source ~/.bashrc
 fi
 echo "now moving on to installing Downlord's FAF..."
-
+echo "[$(date --rfc-3339=seconds)] installing DOWNLORD" >> ~/'fafstack-'$STACKVERSION'.log'
 ENVIRONMENT=$( cat /etc/environment )
 if [ ! -f /etc/environment ] || [ 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"' == "$ENVIRONMENT" ]
 then
     echo "environment is vanilla..."
     echo "proceeding..."
+    echo "[$(date --rfc-3339=seconds)] optimal use-case" >> ~/'fafstack-'$STACKVERSION'.log'
     sudo rm /etc/environment
     sudo echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/jdk-10.0.2/bin:/home/'$USER'/.steam/compatibilitytools.d/Proton/dist/bin:/home/'$USER'/.steam/ubuntu12_32/steam-runtime/amd64/usr/bin"' >> /etc/environment
     sudo echo 'JAVA_HOME="/usr/lib/jvm/jdk-10.0.2"' >> /etc/environment
@@ -120,6 +145,7 @@ then
 else
     if (whiptail --title "Entered : \"Java 10 present but evironement is non-vanilla use-case\"" --yesno "Overwrite /etc/environment? \n\user config that you may have set yourself was discovered in environment. is this keep-worthy? \n\"No\" will skip this \n\"Yes\" will delete and replace /etc/environment \n(keep in mind this script was written by a donkey...)" 11 100)
     then
+    echo "[$(date --rfc-3339=seconds)] sub-optimal use-case, corrected /etc/environment" >> ~/'fafstack-'$STACKVERSION'.log'
     sudo rm /etc/environment
     sudo echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/jdk-10.0.2/bin:/home/'$USER'/.steam/compatibilitytools.d/Proton/dist/bin:/home/'$USER'/.steam/ubuntu12_32/steam-runtime/amd64/usr/bin"' >> /etc/environment
     sudo echo 'JAVA_HOME="/usr/lib/jvm/jdk-10.0.2"' >> /etc/environment
@@ -129,6 +155,7 @@ else
     else
         echo "You're probably right to choose this..."
         echo "you'll have to check /etc/environment 's variables yourself at the end of this script."
+    echo "[$(date --rfc-3339=seconds)] user chose to leave /etc/environement untouched, this might be the cause." >> ~/'fafstack-'$STACKVERSION'.log'
 	fi
 fi
 cd
@@ -151,12 +178,14 @@ echo 'export WINEDLLPATH=/home/'$USER'/.steam/compatibilitytools.d/Proton/dist/l
 echo 'export WINEPREFIX=/home/'$USER'/.steam/steam/steamapps/compatdata/9420/pfx/' >> ~/.bashrc
 echo 'export SteamGameId=9420' >> ~/.bashrc
 echo 'export SteamAppId=9420' >> ~/.bashrc
-source .bashrc
+source ~/.bashrc
 
 echo "launching FAF"
-gnome-terminal -e 'sh -c "cd ~/faf; timeout -k 5 30 ./downlords-faf-client"'
+echo "[$(date --rfc-3339=seconds)] FAF installed, launching FAF..." >> ~/'fafstack-'$STACKVERSION'.log'
+gnome-terminal --tab --active -- bash -c "cd ~/faf; timeout -k 5 30 ./downlords-faf-client"
 #sed -i '17d' /home/$USER/.faforever/client.prefs
 #sed -i 's/"autoDownloadMaps": true,/"autoDownloadMaps": true/g' /home/$USER/.faforever/client.prefs
+sleep 30
 sed -i '/"preferencesFile": "\/home\/'$USER'\/.wine\/drive_c\/users\/'$USER'\/Application Data\/Gas Powered Games\/Supreme Commander Forged Alliance\/Game.prefs"/c\
     "path": "\/home\/'$USER'\/.steam\/steam\/steamapps\/common\/Supreme Commander Forged Alliance",\
     "preferencesFile": "\/home\/'$USER'\/.steam\/steam\/steamapps\/compatdata/9420/pfx/drive_c\/users\/steamuser\/Local Settings/Application Data\/Gas Powered Games\/Supreme Commander Forged Alliance\/Game.prefs",\
@@ -165,12 +194,15 @@ sed -i '/"preferencesFile": "\/home\/'$USER'\/.wine\/drive_c\/users\/'$USER'\/Ap
 
 
 echo "starting Forged Alliance Download..."
+echo "[$(date --rfc-3339=seconds)] starting Forged Alliance Download..." >> ~/'fafstack-'$STACKVERSION'.log'
 eval "steamcmd +login ${STEAMUSERNAME} ${STEAMPASSWORD} +app_update 9420 validate +quit"
 echo "starting Forged Alliance..."
+echo "[$(date --rfc-3339=seconds)] starting Forged Alliance..." >> ~/'fafstack-'$STACKVERSION'.log'
 steam -applaunch 9420
 
 cd
 echo "making map & mods symbolic links"
+echo "[$(date --rfc-3339=seconds)] Maps & Mods" >> ~/'fafstack-'$STACKVERSION'.log'
 cd ~/.steam/steam/steamapps/common/Supreme\ Commander\ Forged\ Alliance
 ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Maps/ Maps
 ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Mods/ Mods
