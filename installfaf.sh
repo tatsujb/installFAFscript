bb2821cd176d01f0e40a237feb7348ec44980b39
tatsujb <tatsu@hotmail.fr>
2019-03-04 22:38:12 +0100

    version 1.5

--- installfaf.sh
+++ installfaf.sh
@@ -28,16 +28,13 @@ else
     echo steam steam/license note '' | sudo debconf-set-selections
     sudo apt install -y steam
 fi
-
 #########################################################################################################################
 #                                                                                                                       #
 # WIP! have not figured out a way to toggle proton-> [on] via command line, right now                                   #
 # you have to do it yourself when steam starts up while the script is running                                           #
 #                                                                                                                       #
 #########################################################################################################################
-
 echo 'PROTON_NO_ESYNC=1, PROTON_DUMP_DEBUG_COMMANDS=1, PROTON_USE_GALLIUM_NINE=1, PROTON_GALLIUM_NINE_MODULEPATH="/usr/lib/i386-linux-gnu/d3d/d3dadapter9.so.1:/usr/lib/x86_64-linux-gnu/d3d/d3dadapter9.so.1" %command%' > ~/"the contents of this file are to be pasted in the forged alliance properties launch options"
-
 gnome-terminal --tab --active --title="install & run steam, steamcmd, FA" -- bash -c 'echo "installing proton and running steam...";
 echo "expecting you to type in Forged Alliances Launch options";
 echo "reminder : look in your home folder, theres a file there with the contents to be pasted";
@@ -61,43 +58,51 @@ steamcmd +login '$STEAMUSERNAME' '$STEAMPASSWORD' +@sSteamCmdForcePlatformType w
 sleep 2;
 echo "[$(date --rfc-3339=seconds)] running steam cmd finished / exited, starting steam again" >> ~/fafstack-'$STACKVERSION'.log;
 steam -login '$STEAMUSERNAME' '$STEAMPASSWORD' -remember_password -nofriendsui -applaunch 9420 -shutdown;
+echo "FA install done waiting in case it isnt";
+sleep 190;
 echo "making map & mods symbolic links";
 echo "[$(date --rfc-3339=seconds)] Maps & Mods" >> ~/fafstack-'$STACKVERSION'.log;
-if [ -f ~/.steam/steam/SteamApps/common/Supreme\ Commander\ Forged\ Alliance ] ;
-then echo "[$(date --rfc-3339=seconds)] SteamApps SupCom found" >> ~/fafstack-'$STACKVERSION'.log;
+if [ -d ~/.steam/steam/SteamApps ] ;
+then echo "[$(date --rfc-3339=seconds)] SteamApps found" >> ~/fafstack-'$STACKVERSION'.log;
+if [ -d ~/.steam/steam/SteamApps/common/Supreme* ] ;
+then echo "[$(date --rfc-3339=seconds)] Supreme Commander folder found" >> ~/fafstack-'$STACKVERSION'.log;
 cd ~/.steam/steam/SteamApps/common/Supreme\ Commander\ Forged\ Alliance;
 ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Maps/ Maps;
 ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Mods/ Mods;
-else echo "[$(date --rfc-3339=seconds)] SteamApps SupCom not found" >> ~/fafstack-'$STACKVERSION'.log;
+else echo "[$(date --rfc-3339=seconds)] Supreme Commander folder not found" >> ~/fafstack-'$STACKVERSION'.log;
 fi;
-if [ -f ~/.steam/steam/steamapps/common/Supreme\ Commander\ Forged\ Alliance ] ;
-then echo "[$(date --rfc-3339=seconds)] steamapps SupCom found" >> ~/fafstack-'$STACKVERSION'.log;
-cd ~/.steam/steam/steamapps/common/Supreme\ Commander\ Forged\ Alliance;
-ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Maps/ Maps;
-ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Mods/ Mods;
-else echo "[$(date --rfc-3339=seconds)] steamapps SupCom not found" >> ~/fafstack-'$STACKVERSION'.log;
-fi;
-if [ -f ~/.steam/steam/SteamApps/common/Supreme\ Commander\ Forged\ Alliance ] ;
-then echo "[$(date --rfc-3339=seconds)] SteamApps compatdata found" >> ~/fafstack-'$STACKVERSION'.log;
+if [ -d ~/.steam/steam/steamapps/compatdata/9420/pfx/drive_c/users/steamuser ] ;
+then echo "[$(date --rfc-3339=seconds)] steamuser profile folder found" >> ~/fafstack-'$STACKVERSION'.log;
 cd ~/.steam/steam/SteamApps/compatdata/9420/pfx/drive_c/users/steamuser;
 rm -rf My\ Documents;
 mkdir My\ Documents;
 cd My\ Documents;
 ln -s ~/My\ Games/ My\ Games;
-else echo "[$(date --rfc-3339=seconds)] SteamApps compatdata not found" >> ~/fafstack-'$STACKVERSION'.log;
+else echo "[$(date --rfc-3339=seconds)] steamuser profile folder found" >> ~/fafstack-'$STACKVERSION'.log;
 fi;
-if [ -f ~/.steam/steam/steamapps/common/Supreme\ Commander\ Forged\ Alliance ] ;
-then echo "[$(date --rfc-3339=seconds)] steamapps compatdata found" >> ~/fafstack-'$STACKVERSION'.log;
+else echo "[$(date --rfc-3339=seconds)] SteamApps not found" >> ~/fafstack-'$STACKVERSION'.log;
+fi;
+if [ -d ~/.steam/steam/steamapps ] ;
+then echo "[$(date --rfc-3339=seconds)] steamapps found" >> ~/fafstack-'$STACKVERSION'.log;
+if [ -d ~/.steam/steam/SteamApps/common/Supreme* ] ;
+then echo "[$(date --rfc-3339=seconds)] Supreme Commander folder found" >> ~/fafstack-'$STACKVERSION'.log;
+cd ~/.steam/steam/steamapps/common/Supreme\ Commander\ Forged\ Alliance;
+ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Maps/ Maps;
+ln -s ~/My\ Games/Gas\ Powered\ Games/Supreme\ Commander\ Forged\ Alliance/Mods/ Mods;
+else echo "[$(date --rfc-3339=seconds)] Supreme Commander folder not found" >> ~/fafstack-'$STACKVERSION'.log;
+fi;
+if [ -d ~/.steam/steam/steamapps/compatdata/9420/pfx/drive_c/users/steamuser ] ;
+then echo "[$(date --rfc-3339=seconds)] steamuser profile folder found" >> ~/fafstack-'$STACKVERSION'.log;
 cd ~/.steam/steam/steamapps/compatdata/9420/pfx/drive_c/users/steamuser;
 rm -rf My\ Documents;
 mkdir My\ Documents;
 cd My\ Documents;
 ln -s ~/My\ Games/ My\ Games;
-else echo "[$(date --rfc-3339=seconds)] steamapps compatdata not found" >> ~/fafstack-'$STACKVERSION'.log;
-fi'
-
-if [ $(command -v lib32gcc1) ]
-then
+else echo "[$(date --rfc-3339=seconds)] steamuser profile folder found" >> ~/fafstack-'$STACKVERSION'.log;
+fi;
+else echo "[$(date --rfc-3339=seconds)] steamapps not found" >> ~/fafstack-'$STACKVERSION'.log;
+fi;
+mv ~/.steam/compatibilitytools.d/'$PROTONNAME' ~-
     echo "[$(date --rfc-3339=seconds)] lib32gcc1 is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
     echo "lib32gcc1 is already installed, proceeding..."
 else
@@ -105,7 +110,6 @@ else
     echo "[$(date --rfc-3339=seconds)] lib32gcc1 was not yet installed, installing..." >> ~/'fafstack-'$STACKVERSION'.log'
     sudo apt install -y lib32gcc1
 fi
-
 if [ $(command -v steamcmd) ]
 then
     echo "[$(date --rfc-3339=seconds)] steam CMD is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
@@ -122,10 +126,8 @@ fi
 echo "[$(date --rfc-3339=seconds)] installing dependencies" >> ~/'fafstack-'$STACKVERSION'.log'
 echo "starting Forged Alliance Download..."
 echo "[$(date --rfc-3339=seconds)] starting Forged Alliance Download..." >> ~/'fafstack-'$STACKVERSION'.log'
-
 echo "got to after main segment"
 sudo apt install -y libd3dadapter9-mesa:i386 libd3dadapter9-mesa &&
-
 if [ $(command -v curl) ]
 then
     echo "[$(date --rfc-3339=seconds)] curl is already installed, proceeding..." >> ~/'fafstack-'$STACKVERSION'.log'
@@ -137,25 +139,18 @@ else
 fi
 sudo apt autoremove -y &&
 sudo apt autoclean &&
-
-
-
-
 echo "Now probing the Java status of this OS..."
 echo "[$(date --rfc-3339=seconds)] Now probing the Java status of this OS..." >> ~/'fafstack-'$STACKVERSION'.log'
-
 if [[ $(command -v java) ]] || [[ $(type -p java) ]] || [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]
 then
     echo "UH-OH, Java is already installed!"
     echo "This is suboptimal, crossing fingers for correct version Java version (E.G. version number 10)..."
     echo "[$(date --rfc-3339=seconds)] JAVA already installed!" >> ~/'fafstack-'$STACKVERSION'.log'
-
     if [ $(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -f1 -d'.') == "10" ]
     then
 	echo "Huzzah! you have the correct version of Java, here's hoping for the best!"
 	echo "Checking, if .bashrc config is also correct..."
 	echo "[$(date --rfc-3339=seconds)] specifically JAVA 10." >> ~/'fafstack-'$STACKVERSION'.log'
-
 	if grep -q "export INSTALL4J_JAVA_HOME=/usr/lib/jvm/jdk-10.0.2" ~/.bashrc
 	then
 	    echo "Huzzah! Java looks all set, let's move on..."
@@ -163,7 +158,6 @@ then
         else
             echo "Hmmm... we're not looking to hot on .bashrc, let's see if we can fix that..."
 	    echo "[$(date --rfc-3339=seconds)] but not correct config." >> ~/'fafstack-'$STACKVERSION'.log'
-
             if (whiptail --title "Entered : \"Java 10 present but .bashrc not correctly configured use-case\"" --yesno "Attempt .bashrc automated correct? \n\"No\" will close this script \n\"Yes\" will automatically edit bashrc \n(keep in mind this script was written by a donkey...)" 10 100)
 	    then
 		echo "OK! \".bashrc\" edited!"
@@ -178,7 +172,6 @@ then
 	    fi           
         fi
     else
-
 	if [ ! -f /usr/lib/jvm ]
 	then
 	    echo "your Path is defined but java is not in /usr/lib/jvm "
@@ -209,9 +202,6 @@ else
     echo "export INSTALL4J_JAVA_HOME=/usr/lib/jvm/jdk-10.0.2" >> ~/.bashrc
     source ~/.bashrc
 fi
-
-
-
 ENVIRONMENT=$(cat /etc/environment)
 RESOLVEDPATH='PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/jdk-10.0.2/bin:/home/'$USER'/.steam/compatibilitytools.d/Proton/dist/bin:/home/'$USER'/.steam/ubuntu12_32/steam-runtime/amd64/usr/bin\"'
 RESOLVEDJAVA='JAVA_HOME=\"/usr/lib/jvm/jdk-10.0.2\"'
@@ -242,8 +232,6 @@ else
         echo "[$(date --rfc-3339=seconds)] user chose to leave /etc/environment untouched, this might be the cause." >> ~/'fafstack-'$STACKVERSION'.log'
     fi
 fi
-
-
 echo "now moving on to installing Downlord's FAF..."
 echo "[$(date --rfc-3339=seconds)] installing DOWNLORD" >> ~/'fafstack-'$STACKVERSION'.log'
 cd
@@ -258,7 +246,6 @@ rmdir downlords-faf-client-${FAFVERSIONNUMBER:1}
 rm _dfc_unix_$FAFVERSION.tar.gz
 chmod +x downlords-faf-client && chmod +x lib/faf-uid
 cd
-mv ~/.steam/compatibilitytools.d/'$PROTONNAME' ~/.steam/compatibilitytools.d/Proton
 cd ~/faf
 ./downlords-faf-client
 cd
