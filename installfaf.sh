e815f1fecec4ea855c062ae5f4d573d3d27f54f9
tatsujb <tatsu@hotmail.fr>
2019-03-05 17:29:01 +0100

    version 1.7

--- installfaf.sh
+++ installfaf.sh
@@ -346,7 +346,7 @@ else
     echo "Java 10 installation procedure..."
     echo "[$(date --rfc-3339=seconds)] JAVA 10 installing..." >> ~/'fafstack-'$STACKVERSION'.log' 
     wget https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz
-    sudo mkdir /usr/lib/jvm
+    sudo mkdir -p /usr/lib/jvm
     cd /usr/lib/jvm
     sudo tar xzf ~/openjdk-10.0.2_linux-x64_bin.tar.gz
     cd
