ec47d93e97878f9604c4362ae092e346f309a0ce
tatsujb <tatsu@hotmail.fr>
2019-04-19 14:06:02 +0200

    version 2.3 (includes custom install dir & externalized sudo & more)
     - fix missing '&' for elementary and kde
     - make sure xterm is installed for non ubuntu systems
     - fixed script not continuing after steam finalizes FA install (x2)
     - fixed a bug making externalized sudo reapear anyways 
     - fixed steam not running bug
     - added better arch support 
     - provided a solution for the problematic situation of sudo on a script
     - support for FA (and all other to-installs and dependencies) already present on host's PC
     - support for custom FA location
     - beginning to support protontricks + winetricks galliumnine & d3d

--- faf.sh
+++ faf.sh
@@ -402,10 +402,10 @@ else
         eval "$gnome_opening_sudo_script $sudo_script"
     elif [ "$operating_system" = "Kubuntu" ]
     then
-        eval "$konsole_opening_sudo_script $sudo_script"
+        eval "$konsole_opening_sudo_script $sudo_script" &
     elif [ "$operating_system" = "elementary OS" ] # elementary's acting up. have to resort to xterm
     then
-        eval "$xterm_opening_sudo_script $sudo_script"
+        eval "$xterm_opening_sudo_script $sudo_script" &
     elif [ "$operating_system" = "Debian GNU/Linux" ]
     then
         eval "$debian_opening_sudo_script $sudo_script"
